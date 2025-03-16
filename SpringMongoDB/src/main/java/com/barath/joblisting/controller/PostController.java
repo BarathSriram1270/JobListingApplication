a400d384e492642ed030f0c28fae6300082c94cb
Barathsriram <133561863+BarathSriram1270@users.noreply.github.com>
2025-03-16 15:51:08 +0530

    Update PostController.java

--- SpringMongoDB/src/main/java/com/barath/joblisting/controller/PostController.java
+++ SpringMongoDB/src/main/java/com/barath/joblisting/controller/PostController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://your-frontend-domain.com"})
 public class PostController
 {
 
